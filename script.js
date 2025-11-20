aba6239651493be99b1607d46ce384d699765a8d
Doumbia <aboubakarnoursery@gmail.com>
2025-11-20 15:59:10 +0000

    mise à jour2

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-let questions = document
+let text_question = document.getElementById("question");
+let propositon = document.querySelectorAll(".proposition_container span")
+let list_question = []
+
+console.log(text_question );
+console.log(propositon );
+
+
